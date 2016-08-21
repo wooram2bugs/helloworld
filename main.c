dd1d7aa129dfea7ac236c632ee92d1fd9cd1543e
wooram88 <wooram881207@gmail.com>
2016-08-21 22:43:30 +0900

    fixed #1 스켈레톤 작성

---
+++ main.c
@@ -0,0 +1,14 @@
+#include
+
+int add(int, int);
+int sub(int, int);
+ind div(int, int);
+int multi(int, int);
+
+int main(int argc, char* argv[])
+{
+	int a, b, c;
+	char operator;
+
+	printf("please enter a expression");	
+}
